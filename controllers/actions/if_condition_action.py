5ed76f066a03200f8dcb4f02dc7b8a4c3e454151
TUANKAKA <ltrtuan@gmail.com>
2025-09-01 22:10:54 +0700

    done if

--- controllers/actions/if_condition_action.py
+++ controllers/actions/if_condition_action.py
@@ -27,6 +27,8 @@ class IfConditionAction(BaseAction):
         
         return False  # Luôn trả về False để ActionController không xử lý thêm
     
+
+
     def _execute_if_block(self):
         """Thực thi các action trong khối IF"""
         all_actions = self.model.get_all_actions()
@@ -44,21 +46,24 @@ class IfConditionAction(BaseAction):
         
             if action.action_type == ActionType.IF_CONDITION:
                 nesting_level += 1
-                # THÊM: Xử lý IF lồng
+                # Xử lý IF lồng
                 self._handle_nested_if(action, i)
             
-                # Nhảy đến END_IF của IF lồng
+                # Tìm END_IF tương ứng với IF lồng này
                 inner_level = 1
-                for j in range(i + 1, len(all_actions)):
+                j = i + 1
+                while j < len(all_actions) and inner_level > 0:
                     inner_action = all_actions[j]
                     if inner_action.action_type == ActionType.IF_CONDITION:
                         inner_level += 1
                     elif inner_action.action_type == ActionType.END_IF_CONDITION:
                         inner_level -= 1
-                        if inner_level == 0:
-                            i = j  # Nhảy đến END_IF của IF lồng
-                            break
-                        
+                    j += 1
+            
+                # Đặt i = j để bỏ qua toàn bộ khối IF lồng
+                i = j
+                continue  # Bỏ qua phần increment cuối vòng lặp
+            
             elif action.action_type == ActionType.END_IF_CONDITION:
                 if nesting_level == 0:
                     break  # Kết thúc khối IF
@@ -71,6 +76,7 @@ class IfConditionAction(BaseAction):
                     self._execute_single_action(action, i)
         
             i += 1
+
 
     
     def _execute_else_block(self):
@@ -119,7 +125,7 @@ class IfConditionAction(BaseAction):
             handler.play()
             
     def _handle_nested_if(self, nested_if_action, nested_index):
-        """Xử lý IF lồng với logic đúng"""
+        """Xử lý IF lồng - chỉ đánh giá điều kiện và để IF tự xử lý"""
         # Tạo handler cho IF lồng
         from controllers.actions.action_factory import ActionFactory
         nested_handler = ActionFactory.get_handler(
@@ -133,63 +139,8 @@ class IfConditionAction(BaseAction):
             if action_frame:
                 nested_handler.action_frame = action_frame
         
-            # Thực thi IF lồng
-            nested_result = nested_handler.play()
-        
-            if nested_result:
-                # IF lồng sai: tìm và thực thi ELSE IF của IF lồng
-                self._execute_nested_else_if(nested_index)
-            else:
-                # IF lồng đúng: thực thi các action trong IF lồng
-                self._execute_nested_if_block(nested_index)
-
-    def _execute_nested_if_block(self, nested_if_index):
-        """Thực thi các action trong IF lồng khi IF lồng đúng"""
-        all_actions = self.model.get_all_actions()
-    
-        i = nested_if_index + 1
-        nesting_level = 0
-    
-        while i < len(all_actions):
-            action = all_actions[i]
-        
-            if action.action_type == ActionType.IF_CONDITION:
-                nesting_level += 1
-            elif action.action_type == ActionType.END_IF_CONDITION:
-                if nesting_level == 0:
-                    break  # Kết thúc IF lồng
-                nesting_level -= 1
-            elif action.action_type == ActionType.ELSE_IF_CONDITION and nesting_level == 0:
-                break  # Gặp ELSE IF của IF lồng, dừng lại
-            else:
-                # Chỉ thực thi action thông thường trong IF lồng
-                if nesting_level == 0:
-                    self._execute_single_action(action, i)
-        
-            i += 1
-
-    def _execute_nested_else_if(self, nested_if_index):
-        """Tìm và thực thi ELSE IF của IF lồng"""
-        all_actions = self.model.get_all_actions()
-    
-        i = nested_if_index + 1
-        nesting_level = 0
-    
-        while i < len(all_actions):
-            action = all_actions[i]
-        
-            if action.action_type == ActionType.IF_CONDITION:
-                nesting_level += 1
-            elif action.action_type == ActionType.END_IF_CONDITION:
-                if nesting_level == 0:
-                    break  # Hết IF lồng, không có ELSE IF
-                nesting_level -= 1
-            elif action.action_type == ActionType.ELSE_IF_CONDITION and nesting_level == 0:
-                # Tìm thấy ELSE IF của IF lồng, thực thi nó
-                self._execute_single_action(action, i)
-                return
-        
-            i += 1
+            # Chỉ gọi play() và để IF lồng tự xử lý logic của nó
+            nested_handler.play()
 
     
     def _find_current_index(self, all_actions):
